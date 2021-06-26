8cc3154d529f3b98cefedd10d08aca0cc358aede
figurec <86502121+figurec@users.noreply.github.com>
2021-06-26 10:51:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,22 @@
  *          Dante - The Divine Comedy (Canto III)
  */
 
-module.exports = require('./lib/http-proxy');
+//module.exports = require('./lib/http-proxy');
+
+PORT = process.env.PORT || 8000;
+
+var http = require('http'),
+    httpProxy = require('http-proxy');
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(PORT); // See (†)
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9000);
